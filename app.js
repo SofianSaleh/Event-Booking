710a7f82a0dc6a7b65e62f018cc836b9f7c95f6f
SofianSaleh <sofiane.saleh11@egmail.com>
2020-03-22 13:02:50 +0200

    started with graphQL

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const graphQlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res, next) => {
-  res.send("HELLO WORLD");
-});
+app.use(
+  "/graphql",
+  graphQlHttp({
+    schema: buildSchema(`
+        schema{
+            query:
+            mutation
+        }
+    `),
+    rootValue: {}
+  })
+);
 
 app.listen(port, () => `your are listening at the port ${port}`);
